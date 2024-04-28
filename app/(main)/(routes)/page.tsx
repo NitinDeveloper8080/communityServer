d804f798fa686a8d03376b979934e84871fcb808
Nitin Pal <nitinpal2d@gmail.com>
2024-04-28 11:32:01 +0530

    added darkmode functionality

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
@@ -6,10 +7,14 @@ export default function Home() {
   return <>
     <div>
 
-    <h1 className="text-center font-bold text-green-900">Community Server welcomes you to explore its new features </h1>
+    <h1 className="text-center font-bold text-black">Community Server welcomes you to explore its new features </h1>
 <UserButton
 afterSignOutUrl="/"
 />
+<div className="flex justify-end align-items-start"> 
+
+<ModeToggle />
+</div>
     </div>
   </>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Community Server",
@@ -17,8 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className,"bg-white dark:bg-[#313338]")}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        storageKey="community-theme"
+        enableSystem={false}>
+        {children}
+        </ThemeProvider>
+        
+        </body>
     </html>
     </ClerkProvider>
   );
